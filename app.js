0a573a792951d323b8e9a61b230ada6492ea62e9
milan.zivanov <milan.zivanov@gmail.com>
2021-08-10 12:08:27 +0200

    Add Express and Pug

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // export AIRTABLE_API_KEY=keyYtAo04su8CVgyb;
-
-
 const base = require("airtable").base("appWKP80d1ItnOZnw");
+const express = require("express");
+const path = require("path");
 
-(async () => {
-    const records = await base("Business Hours")
-    .select({
-      view: "Grid view",
-    })
-    .firstPage()
+const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "."));
 
-    for (const record of records) {
-        // console.log(record.fields);
-        console.log(record.get("Day"), record.get("Hours"))
-    }
-})();
+app.get("/", (req, res) => {
+    //your existing code
+    (async () => {
+        const records = await base("Business Hours")
+        .select({
+          view: "Grid view",
+        })
+        .firstPage()
+    
+        res.render('page', {
+            records,
+        });
+
+    })();
+});
+
+app.listen(3000, () => console.log("Server ready"));
+
